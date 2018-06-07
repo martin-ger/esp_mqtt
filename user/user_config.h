f1050e5b271e74a544a7c2884b4c27a25d8ff574
Martin Ger <martin@kawa.fritz.box>
2018-06-07 08:40:48 +0200

    Fix in init_data

--- user/user_config.h
+++ user/user_config.h
@@ -1,7 +1,7 @@
 #ifndef _USER_CONFIG_
 #define _USER_CONFIG_
 
-#define ESP_UBROKER_VERSION "V2.0.2"
+#define ESP_UBROKER_VERSION "V2.0.3"
 
 #define WIFI_SSID            "ssid"
 #define WIFI_PASSWORD        "password"
